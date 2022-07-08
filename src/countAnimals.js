621295374eaf2def8baed4da89037ac8b854b390
Hian <hian.hsm@gmail.com>
2022-07-08 08:34:49 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
 function countAnimals(animal) {
   // seu código aqui
+  const allAnimals = species.reduce((acc, { name, residents }) => (
+    { ...acc, [name]: residents.length }), {});
+  if (!animal) return allAnimals;
+  const { specie, sex } = animal;
+  const selectedAnimal = species.find((animals) => animals.name === specie);
+  const selectedSex = selectedAnimal.residents.filter((animalSpecie) => animalSpecie.sex === sex);
+  return !sex ? selectedAnimal.residents.length : selectedSex.length;
 }
 
 module.exports = countAnimals;
