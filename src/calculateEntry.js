34d1a1a2f16f40152aa3978122ed625cdecda148
Hian <hian.hsm@gmail.com>
2022-07-09 13:34:21 -0300

    Requisitos 8 e 10

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,18 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  const countPeople = entrants.map((person) => person.age);
+  const countChild = countPeople.filter((age) => age < 18);
+  const countAdult = countPeople.filter((age) => age >= 18 && age < 50);
+  const countSenior = countPeople.filter((age) => age >= 50);
+  return { child: countChild.length, adult: countAdult.length, senior: countSenior.length };
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (!entrants || !Object.values(entrants).length) return 0;
+  const people = countEntrants(entrants);
+  return people.child * 20.99 + people.adult * 49.99 + people.senior * 24.99;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const employeeSpecies = employees.find((person) => person.id === id).responsibleFor;
+  const firstSpecie = species.find((animal) => animal.id === employeeSpecies[0]).residents;
+  const oldestAnimal = firstSpecie.reduce((max, specie) => {
+    if (max.age > specie.age) return max;
+    return specie;
+  });
+  return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
 }
+
+getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1');
 
 module.exports = getOldestFromFirstSpecies;
